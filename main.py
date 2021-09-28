50247a3749487d21731a91c120c0cc6aff65a35d
moridaiki <dm.futsal.n@gmail.com>
2021-09-28 23:34:51 +0900

    add: background segmentation

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ def get_perspective_transform():
 def video_capture():
 
     window_name = 'win'
+    mask_name = 'mask'
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.namedWindow(mask_name, cv2.WINDOW_NORMAL)
 
     cap = cv2.VideoCapture(2)
 
@@ -21,6 +23,8 @@ def video_capture():
         return
 
     M = get_perspective_transform()
+    subtractor = cv2.bgsegm.createBackgroundSubtractorMOG()
+
 
     while True:
         ret, frame = cap.read()
@@ -31,6 +35,15 @@ def video_capture():
             key = cv2.waitKey(1)
             if key & 0xFF == ord('q'):
                 break
+            elif key & 0xFF == ord('m'):
+                mask = subtractor.apply(frame)
+                detect_contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+                detect_contours = list(filter(lambda x: cv2.contourArea(x) > frame.shape[0] * frame.shape[1] * (1/500), detect_contours))
+                detect_bboxes = list(map(lambda x: cv2.boundingRect(x), detect_contours))
+                print(detect_bboxes)
+                for x, y, w, h in detect_bboxes:
+                    cv2.rectangle(mask, (x, y), (x+w, y+h), (255), 2)
+                cv2.imshow(mask_name, mask)
 
 
 
